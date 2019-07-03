485c000bf4f3b8ddf0c34a51b4f4fc69375ae92d
Yemre <aybey77@gmail.com>
2019-07-03 18:29:30 +0300

    Post Model Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     validates :name, presence: true, uniqueness: true, length: {maximum:20}
+    has_many  :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_03_145300) do
+ActiveRecord::Schema.define(version: 2019_07_03_151652) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
